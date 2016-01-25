deb49c65ba23324651c354a627878e02b13946ed
SWEKOSKY <Nick.Swekosky@bjc.org>
2016-01-25 13:45:44 -0600

    MASTER Created schema for a product and a product-category

---
+++ models/product.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ProductSchema = new Schema({
+
+	category: { type: Schema.types.ObjectId, ref: 'Category'},
+    	name: String,
+    	price: Number,
+    	image: String
+
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
